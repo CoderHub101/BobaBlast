578808fa5a0caf84863bf687301058fb21bf8400
iTakedown27 <ezrabridger27@gmail.com>
2024-02-01 14:22:46 -0800

    initial commit

---
+++ demo/src/main/java/com/coderhub/demo/BobaBlastApplication.java
@@ -0,0 +1,13 @@
+package com.coderhub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobaBlastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobaBlastApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/coderhub/demo/BobaBlastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobaBlastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
